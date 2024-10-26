713bade829b4dabe68be356e377838315f702bac
emilykamiti <emilykamiti@gmail.com>
2024-10-26 14:37:33 +0300

    Connecting to the db

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+//dataimports
+import User from "./model/User.js";
+import { dataUser } from "./data/index.js";
+
 //configurations
 dotenv.config();
 const app = express();
@@ -38,5 +42,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`server Port: ${PORT}`));
+
+    //ONLY ADD DATA ONE TIME
+    User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
